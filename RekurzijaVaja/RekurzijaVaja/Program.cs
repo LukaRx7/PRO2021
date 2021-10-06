7590bddf9a1ad5015f81fd6bb934906245cded0f
Luka <luka.zailacdohc@gmail.com>
2021-10-06 17:51:43 +0200

    Vaje 3,4,5.

--- RekurzijaVaja/RekurzijaVaja/Program.cs
+++ RekurzijaVaja/RekurzijaVaja/Program.cs
@@ -12,7 +12,10 @@ namespace RekurzijaVaja
         {
             Console.WriteLine("2 na 10 je " + PotencaIterativno(10));
             Console.WriteLine("2 na 10 je " + PotencaRekurzivno(10));
-            Console.WriteLine("7 Fib stevilo " + Fib(250));
+            Console.WriteLine("10 Fib stevilo " + Fib(10));
+            Console.WriteLine("5 mesto v zaporedju je " +Vaja3(5));
+            Console.WriteLine("4 mesto v zaporedju je " + Vaja4(4));
+            Console.WriteLine("4 mesto v zaporedju je " + Vaja5(4));
             Console.ReadLine();
         }
         static int Fib(int n)
@@ -38,9 +41,31 @@ namespace RekurzijaVaja
                 return 1;
             return 2 * PotencaRekurzivno(n - 1);
         }
+        //VAJA 3
         static int Vaja3(int n)
         {
-
+            if (n == 1 || n == 2)
+                return 2;
+            else
+                return Vaja3(n - 2) * Vaja3(n - 1) -1;
+        }
+        //VAJA 4
+        static int Vaja4(int n)
+        {
+            if (n == 1)
+                return 2;
+            else
+                return 3 * Vaja4(n - 1) + 2;
+        }
+       //VAJA 5
+        static int Vaja5(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            else
+                return Vaja5(n - 1) * 2 + Vaja5(n - 2);
         }
     }
 }
